e638e021f8ac9bcf1a7a5a7044543be07664c694
jpcallanta <jp@callanta.net>
2017-11-22 15:03:56 -0500

    Adding commands for run one and run forever

--- dirtyrobocop.go
+++ dirtyrobocop.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: dirtyrobocop <config_path> <runonce|runforever>")
+		os.Exit(1)
+	}
+
 	pullConfig(os.Args[1])
+
 	twitter := initTwitter()
 
-	// Main loop forever!
-	for {
-		snore(config.SleepMin, config.SleepMax)
+	switch os.Args[2] {
+	case "runonce":
 		tweetSomething(twitter)
+
+	case "runforever":
+		// Main loop forever!
+		for {
+			snore(config.SleepMin, config.SleepMax)
+			tweetSomething(twitter)
+		}
 	}
 }
